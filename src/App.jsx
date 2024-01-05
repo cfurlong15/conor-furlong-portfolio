ac3306dd863b4dfa2e04731670179d220d2e817a
Conor Furlong <cfurlong15@gmail.com>
2024-01-04 22:14:58 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './sections/About'
+import Projects from './sections/Projects'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Header />
+      <h1>Conor Furlong Portfolio</h1>
+      <About />
+      <Projects />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/sections/About.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+//import "./About.css";
+
+function About() {
+  const [about, setAbout] = useState(null);
+
+  const getAboutData = async () => {
+    const response = await fetch("./about.json");
+    const data = await response.json();
+    setAbout(data);
+  };
+
+  useEffect(() => {
+    getAboutData();
+  }, []);
+
+  const loaded = () => (
+    <div className="About">
+      <h1 className="aboutMe">About Me</h1>
+
+      <p className="Bio">{about.bio}</p>
+      <p className="Email">Contact me at {about.email}</p>
+      <div className="Socials">
+        <a className="Linkedin" href={about.linkedin}>
+          <button>LinkedIn</button>
+        </a>
+        <a className="Github" href={about.github}>
+          <button>GitHub</button>
+        </a>
+      </div>
+    </div>
+  );
+
+  return about ? loaded() : <h1>Loading...</h1>;
+}
+
+export default About;
